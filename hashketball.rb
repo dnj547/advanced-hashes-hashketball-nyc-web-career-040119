9523ee053991b1f88d5958bf73d384167f03bcd9
Danielle Jasper <dnj547@live.com>
2019-03-13 18:22:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,13 +185,12 @@ def player_numbers(team)
 end
 
 def player_stats(player)
-  stats = {}
   game_hash.each do |location, team_data|
     team_data.each do |data_type, data|
       if data_type.to_s == "players"
         data.each do |player_name, stat|
           if player_name == player
-            stats << stat
+            player_name.values
           end
         end
       end
