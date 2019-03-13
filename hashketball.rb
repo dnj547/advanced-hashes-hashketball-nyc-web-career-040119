feb8da7d49879b2b6810dd782f574a60393061e2
Danielle Jasper <dnj547@live.com>
2019-03-13 19:04:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,7 @@ def num_points_scored(player)
   data = iterate_through_game_hash("players")
   data.each do |player_name, stat|
     if player_name == player
-      return player[stat][:points]
+      return stat
     end
   end
 end
